b9896fb4fc8b415e02663d879e4525e981bbd455
Evgeniy Tatarkin <tatarkin.evg@gmail.com>
2012-05-06 12:17:09 +0400

    new release

--- sadisplay/__init__.py
+++ sadisplay/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.3.2dev'
+__version__ = '0.3.3dev'
 
 from sadisplay.describe import describe     # pyflakes:ignore
 from sadisplay.render import plantuml, dot  # pyflakes:ignore
