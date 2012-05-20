c26c2cfcbb2b09fbd8c470f8f15416e6dd766d3d
Evgeniy Tatarkin <tatarkin.evg@gmail.com>
2012-05-20 15:00:39 +0400

    bug fix release

--- sadisplay/__init__.py
+++ sadisplay/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.3.4dev'
+__version__ = '0.3.5dev'
 
 from sadisplay.describe import describe     # pyflakes:ignore
 from sadisplay.render import plantuml, dot  # pyflakes:ignore
